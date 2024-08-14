f3a8179ebcf242753b54f7923b3e6c60e6712eef
Vassu Mehta <100787454+Vasu1601@users.noreply.github.com>
2024-08-14 23:45:31 +0530

    Bubble Sort

---
+++ 06 Bubble Sort/01 Code/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        int[] arr = {-32, 56, 22, -15, 0};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr){
+        boolean swapped;
+
+        for(int i = 0; i<arr.length; i++){
+
+            swapped = false;
+            for(int j = 1; j < arr.length - i; j++){
+
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] =temp;
+                    swapped = true;
+                }
+
+            }
+
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}
